e6e9d91d60bf3eed28725f8a9709f8899deb9bb4
Scott Riley <scott.riley111@gmail.com>
2016-04-07 16:16:35 +0100

    Initial commit with folder structure

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+
+app.use(express.static('../client/build'));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
