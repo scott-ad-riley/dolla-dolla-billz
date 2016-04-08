e507b5cd6a53180bf44a9d71eafc39b80b49a856
John Harper <john.rr.harper@gmail.com>
2016-04-08 13:26:58 +0100

    swapped out value from holding.js, also use api on server and remove sample data json

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ var Portfolio = require('./models/portfolio.js');
 var Holding = require('./models/holding.js');
 
 window.onload = function() {
-  var url = '/sample_data.json'
+  var url = '/api/portfolio'
   var userPortfolio = new Portfolio(url);
   userPortfolio.fetch(Holding);
 };
--- client/src/models/holding.js
+++ client/src/models/holding.js
@@ -2,10 +2,10 @@ var Holding = function (info) {
   this.buyPrice = info.buyPrice;
   this.epic = info.epic;
   this.name = info.name;
-  this.ionex = info.ionex;
   this.pastCloseOfDayPrices = info.pastCloseOfDayPrices;
   this.price = info.price;
   this.quantity = info.quantity;
+  this.buyDate = info.buyDate;
 }
 
 module.exports = Holding;
