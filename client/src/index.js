9c90f7e5efa84ad8b56f5dc10538fab612a37d5b
John Harper <john.rr.harper@gmail.com>
2016-04-08 10:58:44 +0100

    portfolio data parsed

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,14 @@
 window.onload = function() {
+  var url = '/sample_data.json'
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
 
+  request.onload = function(){
+    if(request.status === 200){
+      var jsonString = request.responseText;
+      var portfolio = JSON.parse(jsonString);
+      console.log(portfolio)
+    }
+  }
+  request.send();
 };
