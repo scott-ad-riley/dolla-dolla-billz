28ff65927e962ca9fc034e2aa8d57cd63257ab4a
Borxburn-One <broxburn001@yahoo.co.uk>
2016-04-08 11:40:48 +0100

    fixed the type attribute on src tag

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -42,11 +42,61 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var Portfolio = __webpack_require__(1);
+	var Holding = __webpack_require__(2);
+	
+	window.onload = function() {
+	  var url = '/sample_data.json'
+	  var userPortfolio = new Portfolio(url);
+	  userPortfolio.fetch(Holding);
+	};
+
+/***/ },
+/* 1 */
 /***/ function(module, exports) {
 
-	window.onload = function() {
+	var Portfolio = function(url){
+	  this.url = url;
+	  this.holdings = [];
+	  this.fetch = function(holdingConstructor){
+	    var request = new XMLHttpRequest();
+	    request.open("GET", this.url);
 	
-	};
+	    request.onload = function(){
+	      if(request.status === 200){
+	        var jsonString = request.responseText;
+	        var portfolio = JSON.parse(jsonString);
+	        portfolio.forEach(function (holdingData) {
+	          var holdingObject = new holdingConstructor(holdingData);
+	          this.holdings.push(holdingObject);
+	        }.bind(this))
+	        console.log(this.holdings)
+	      }
+	    }.bind(this)
+	    request.send();
+	
+	  }
+	}
+	
+	module.exports = Portfolio;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	var Holding = function (info) {
+	  this.buyPrice = info.buyPrice;
+	  this.epic = info.epic;
+	  this.name = info.name;
+	  this.ionex = info.ionex;
+	  this.pastCloseOfDayPrices = info.pastCloseOfDayPrices;
+	  this.price = info.price;
+	  this.quantity = info.quantity;
+	}
+	
+	module.exports = Holding;
 
 /***/ }
 /******/ ]);
