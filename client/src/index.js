44d9f8a961288923bef5c76c6496d692c269399e
John Harper <john.rr.harper@gmail.com>
2016-04-08 11:15:29 +0100

    portfolio model created

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,7 @@
+var Portfolio = require('./models/portfolio.js')
+
 window.onload = function() {
   var url = '/sample_data.json'
-  var request = new XMLHttpRequest();
-  request.open("GET", url);
-
-  request.onload = function(){
-    if(request.status === 200){
-      var jsonString = request.responseText;
-      var portfolio = JSON.parse(jsonString);
-      console.log(portfolio)
-    }
-  }
-  request.send();
+  var userPortfolio = new Portfolio(url);
+  userPortfolio.fetch();
 };
