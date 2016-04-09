a0979f8902ee9b4c77beb7846efdbf2e1eb3f348
Scott Riley <scott.riley111@gmail.com>
2016-04-09 12:48:37 +0100

    Adds a basic change function with test to calculate percent difference

--- client/src/models/holding.js
+++ client/src/models/holding.js
@@ -12,4 +12,13 @@ Holding.prototype.value = function () {
   return this.price * this.quantity;
 }
 
+Holding.prototype.change = function (distance) {
+    // (Price Sold - Previous Price)/(Previous Price)
+    var distance = distance || 0;
+    var previousPrice = this.pastCloseOfDayPrices[(this.pastCloseOfDayPrices.length - (distance + 1))]
+    var difference = this.price - previousPrice;
+    console.log("calculating", difference, this.price)
+    return ((difference / previousPrice) * 100).toFixed(1);
+}
+
 module.exports = Holding;
