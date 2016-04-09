4836f4fdfa78ab3e89e7aeff31c6915cf8fcbd80
Scott Riley <scott.riley111@gmail.com>
2016-04-09 14:27:19 +0100

    Refactors the holding view for the table

--- client/src/views/holding_view.js
+++ client/src/views/holding_view.js
@@ -1,39 +1,19 @@
+var camelCase = require('../utils/strip_heading.js');
 var HoldingView = function (holdingObj, fields) {
   this.data = holdingObj;
   this.fields = fields;
 
   this.render = function () {
     var tr = document.createElement('tr');
-    
-    var td1 = document.createElement("td");
-    td1.innerHTML = this.data[fields[0]];
-    tr.appendChild(td1);
-
-    var td2 = document.createElement("td");
-    td2.innerHTML = this.data[fields[1]];
-    tr.appendChild(td2);
-
-    var td3 = document.createElement("td");
-    td3.innerHTML = this.data[fields[2]];
-    tr.appendChild(td3);
-
-    var td4 = document.createElement("td");
-    td4.innerHTML = this.data[fields[3]];
-    tr.appendChild(td4);
-
-    var td5 = document.createElement("td");
-    td5.innerHTML = this.data[fields[4]];
-    tr.appendChild(td5);
-
-    var td6 = document.createElement("td");
-    td6.innerHTML = this.data[fields[5]];
-    tr.appendChild(td6);
-
-     var tr7 = document.createElement("td");
-    var value = (td6.innerHTML * td4.innerHTML)/100;
-    tr7.innerHTML = "£" + value;
-    tr.appendChild(tr7);
-
+    this.fields.forEach(function (field) {
+      var td = document.createElement("td");
+      if (typeof field.value === "function") {
+        td.innerHTML = field.value(this.data);
+      } else {
+        td.innerHTML = this.data[camelCase(field.heading)];
+      }
+      tr.appendChild(td);
+    }.bind(this))
     return tr;
   }
 }
--- client/src/views/portfolio_view.js
+++ client/src/views/portfolio_view.js
@@ -1,16 +1,30 @@
 var HoldingView = require('./holding_view.js');
 
+var defaultFields = [
+  { heading:"Name" },
+  { heading:"Epic" },
+  { heading:"Buy Price (p)" },
+  { heading:"Quantity" },
+  { heading:"Buy Date" },
+  { heading:"Price (p)" },
+  {
+    heading:"Value",
+    value: function (holding) {
+      return "£" + (holding.value() / 100).toFixed(2);
+    }
+  }
+];
+
 var PortfolioView = function (portfolioObj, headings) {
   this.data = portfolioObj;
-  this.fields = headings || ["Name","Epic","Buy Price","Quantity","Buy Date", "Price", "Value"];
-  this.properties = ["name","epic","buyPrice","quantity","buyDate", 'price'];
+  this.fields = headings || defaultFields;
   this.render = function () {
     var tableEl = document.createElement("table");
     tableEl.id = "portfolio-table"
     tableEl.appendChild(this.renderHeader());
 
     this.data.forEach(function (holding) {
-      var holdingView = new HoldingView(holding, this.properties);
+      var holdingView = new HoldingView(holding, this.fields);
       tableEl.appendChild(holdingView.render());
     }.bind(this))
 
@@ -20,7 +34,7 @@ var PortfolioView = function (portfolioObj, headings) {
     var tableHeadingRow = document.createElement("tr");
     this.fields.forEach(function (field) {
       var th = document.createElement("th");
-      th.innerHTML = field;
+      th.innerHTML = field.heading;
       tableHeadingRow.appendChild(th);
     })
     return tableHeadingRow;
