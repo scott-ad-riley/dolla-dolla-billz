2555bfd68de49971b0bc84ec627b454cdf296e27
Scott Riley <scott.riley111@gmail.com>
2016-04-11 15:23:31 +0100

    Tests back to passing

--- client/specs/portfolio_specs.js
+++ client/specs/portfolio_specs.js
@@ -5,7 +5,7 @@ var Portfolio = require('../src/models/portfolio.js');
 describe("A Portfolio", function () {
   var holding1, holding2, holding3, userPortfolio;
   beforeEach(function () {
-    holding1 = new Holding({
+    holding1 = {
       "name": "Fusionex",
       "epic":"FXI",
       "price": 120.00,
@@ -13,8 +13,8 @@ describe("A Portfolio", function () {
       "buyPrice": 80.00,
       "pastCloseOfDayPrices": [92.00, 89.00, 103.00, 125.00, 108.00, 98.00, 110.00],
       "buyDate":"2014-11-15"
-    });
-    holding2 = new Holding({
+    };
+    holding2 = {
       "name": "Empiric Student Prop",
       "epic":"ESP",
       "price": 112.00,
@@ -22,8 +22,8 @@ describe("A Portfolio", function () {
       "buyPrice": 100.00,
       "pastCloseOfDayPrices": [90.00, 78.50, 82.50, 110.00, 109.00, 109.00, 110.50],
       "buyDate":"2013-10-23"
-    });
-    holding3 = new Holding({
+    };
+    holding3 = {
       "name": "Worldpay",
       "epic":"WGP",
       "price": 301.00,
@@ -31,9 +31,8 @@ describe("A Portfolio", function () {
       "buyPrice": 209.40,
       "pastCloseOfDayPrices": [232.60, 220.00, 222.00, 221.60, 240.00, 238.00, 235.40],
       "buyDate":"2015-12-22"
-    });
-    userPortfolio = new Portfolio();
-    userPortfolio.holdings = [holding1, holding2, holding3];
+    };
+    userPortfolio = new Portfolio([holding1, holding2, holding3], Holding);
   });
 
   it("should be able to calculate it's total value", function () {
