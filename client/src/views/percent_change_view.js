1f0294636d7cfc39da5c4ff9049b47c317b6a199
Scott Riley <scott.riley111@gmail.com>
2016-04-11 11:41:36 +0100

    removed obsolete console.log

--- client/src/views/percent_change_view.js
+++ client/src/views/percent_change_view.js
@@ -2,7 +2,6 @@ require('short-dom')();
 var formatPercentChange = function (holding) {
   var span = ce('span');
   var percentChange = holding.change();
-  console.log(percentChange)
   if (percentChange > 0) {
     span.innerHTML = "&#x25B2;" + percentChange + "%"; // Gives me the ▲ character
     span.classList.add('green')
