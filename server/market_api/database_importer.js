e11f773e3afd2941f2df227e29b47209ff2d12ca
Scott Riley <scott.riley111@gmail.com>
2016-04-13 17:32:49 +0100

    updates holdings when new market data is imported

--- server/market_api/database_importer.js
+++ server/market_api/database_importer.js
@@ -1,36 +1,88 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/dolla_dolla_db';
 
-var CollectionImporter = function (maxConcRequests, collectionName) {
+var CollectionImporter = function (maxConcRequests) {
   this.max = maxConcRequests;
-  this.collection = collectionName;
   this.currentRequestCount = 0;
-  this.requests = [];
+  this.marketRequests = [];
+  this.portfolioRequests = [];
 }
 
-CollectionImporter.prototype.addRequest = function (objectToInsert) {
+CollectionImporter.prototype.addMarketRequest = function (objectToInsert, callback) {
   if (this.max === this.currentRequestCount) {
-    this.requests.push(objectToInsert);
+    this.marketRequests.push({value: objectToInsert, callback: callback});
   } else {
-    this.insertObject(objectToInsert);
+    this.upsertObject({value: objectToInsert, callback: callback});
   }
 }
 
-CollectionImporter.prototype.insertObject = function (object) {
+CollectionImporter.prototype.addHoldingRequest = function (objectToInsert) {
+  if (this.max === this.currentRequestCount) {
+    this.portfolioRequests.push(objectToInsert);
+  } else {
+    this.updateHolding(objectToInsert);
+  }
+}
+
+CollectionImporter.prototype.upsertObject = function (data) {
   this.currentRequestCount++;
   MongoClient.connect(url, function(err, db) {
       if(err) {
           console.log(err);
           return;
       }
-      var collection = db.collection(this.collection);
-      collection.insert(object);
-      db.close(function () {
-        this.currentRequestCount--;
-        if (this.requests.length > 0) {
-          this.insertObject(this.requests.pop())
-        }
-      }.bind(this))
+      var collection = db.collection('market');
+      collection.update(
+        {
+          "epic" : data.value.epic
+        },
+        data.value,
+        {
+          upsert: true
+        }, function () {
+          db.close(function () {
+            data.callback();
+            this.currentRequestCount--;
+            if (this.marketRequests.length > 0) {
+              this.upsertObject(this.marketRequests.pop())
+            } else if (this.portfolioRequests > 0) {
+              this.updateHolding(this.portfolioRequests.pop())
+            }
+          }.bind(this))
+      }.bind(this));
+      
+  }.bind(this));
+}
+
+CollectionImporter.prototype.updateHolding = function (object) {
+  this.currentRequestCount++;
+  MongoClient.connect(url, function(err, db) {
+      if(err) {
+          console.log(err);
+          return;
+      }
+      var collection = db.collection('portfolio');
+      collection.updateOne(
+        {
+          "epic" : object.epic
+        },
+        {
+          $set : {
+            price: object.price,
+            pastCloseOfDayPrices: object.pastCloseOfDayPrices
+          }
+        },
+        function () {
+          db.close(function () {
+            this.currentRequestCount--;
+            if (this.marketRequests.length > 0) {
+              this.upsertObject(this.marketRequests.pop())
+            } else if (this.portfolioRequests > 0) {
+              this.updateHolding(this.portfolioRequests.pop())
+            }
+          }.bind(this))
+      }.bind(this));
+      
   }.bind(this));
 }
 
--- server/market_api/fetch_and_import_market.js
+++ server/market_api/fetch_and_import_market.js
@@ -3,8 +3,8 @@ var DetailView = require('./detail_view.js');
 var buildDates = require('./build_dates.js');
 var getHistData = require('./get_history_data.js');
 var Stock = require('./stock.js');
-var MarketImporter = require('./database_importer.js');
-var importer = new MarketImporter(190, 'market');
+var DatabaseImporter = require('./database_importer.js');
+var importer = new DatabaseImporter(190);
 var firstGroup = new DetailView(tickers[0])
 var secondGroup = new DetailView(tickers[1])
 var thirdGroup = new DetailView(tickers[2])
@@ -18,7 +18,9 @@ var fetchAndImportMarketData = function () {
       var histDataRequest = new getHistData(stockWithoutHistory.symbol, buildDates[1], buildDates[0])
       histDataRequest.fetch(function (arrayOfStockPastPrices) {
         var stock = new Stock(stockWithoutHistory, arrayOfStockPastPrices);
-        importer.addRequest(stock);
+        importer.addMarketRequest(stock, function () {
+          importer.addHoldingRequest(stock)
+        });
       })
     })
   });
@@ -30,7 +32,9 @@ var fetchAndImportMarketData = function () {
       var histDataRequest = new getHistData(stockWithoutHistory.symbol, buildDates[1], buildDates[0])
       histDataRequest.fetch(function (arrayOfStockPastPrices) {
         var stock = new Stock(stockWithoutHistory, arrayOfStockPastPrices);
-        importer.addRequest(stock);
+        importer.addMarketRequest(stock, function () {
+          importer.addHoldingRequest(stock)
+        });
       })
     })
   });
@@ -42,7 +46,9 @@ var fetchAndImportMarketData = function () {
       var histDataRequest = new getHistData(stockWithoutHistory.symbol, buildDates[1], buildDates[0])
       histDataRequest.fetch(function (arrayOfStockPastPrices) {
         var stock = new Stock(stockWithoutHistory, arrayOfStockPastPrices);
-        importer.addRequest(stock);
+        importer.addMarketRequest(stock, function () {
+          importer.addHoldingRequest(stock)
+        });
       })
     })
   });
