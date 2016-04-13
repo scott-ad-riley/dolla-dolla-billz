72019de3a26afe8f06cde5eff5a7769f4f374587
Peter Singh <singh.prd@gmail.com>
2016-04-13 01:15:35 +0100

    added table to market page and made layout better

--- client/src/index.js
+++ client/src/index.js
@@ -62,10 +62,14 @@ router.route({
     var chartContainer = ce('div');
     container.appendChild(chartContainer);
 
+    var tableContainer = document.createElement('div');
+    left.appendChild(tableContainer);
+
     stocksList.onchange = function() {
       while (right.firstChild) {
           right.removeChild(right.firstChild);
       }
+
       var stockChart = ce('div');
       stockChart.id = "stockChart";
       right.appendChild(stockChart);
@@ -76,6 +80,26 @@ router.route({
       }
       var stockToChart = data.find(findStock); 
       var stockLineGraph = new StockLineChart(stockChart, stockToChart);
+
+      while (tableContainer.firstChild ) {
+          tableContainer.removeChild(tableContainer.firstChild);
+      }
+      var stockTable = document.createElement('table');
+      tableContainer.appendChild(stockTable);
+      console.log(tableContainer)
+      stockTable.id = "stock-table";
+      stockTable.classList.add("pure-table")
+
+      Object.keys(stockToChart).forEach(function(key,index) {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        var td2 = document.createElement('td');
+        td.innerText = key;
+        td2.innerText = stockToChart[key];
+        tr.appendChild(td);
+        tr.appendChild(td2);
+        stockTable.appendChild(tr);
+      });
     };
     stocksList.onchange();
   }
